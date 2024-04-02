e0c15809cbdab90e39724c5f2a02d2f04cc44865
Jason Stewart <support@eggplantsd.com>
2024-04-01 23:29:38 -0400

    removed unused unix-only functions

--- term_misc.go
+++ term_misc.go
@@ -2,13 +2,11 @@ package rasterm
 
 import (
 	"errors"
-	"io"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 
 	"golang.org/x/term"
@@ -178,66 +176,6 @@ func RequestTermAttributes() (sAttrs []int, E error) {
 }
 
 var rxNumber = regexp.MustCompile(`\d+`)
-
-func findPtyDevByStat(pStat *syscall.Stat_t) (string, error) {
-
-	for _, devDir := range []string{"/dev/pts", "/dev"} {
-
-		fd, E := os.Open(devDir)
-		if os.IsNotExist(E) {
-			continue
-		} else if E != nil {
-			return "", E
-		}
-		defer fd.Close()
-
-		for {
-
-			fis, e2 := fd.Readdir(256)
-			for _, fi := range fis {
-
-				if fi.IsDir() {
-					continue
-				}
-
-				if s, ok := fi.Sys().(*syscall.Stat_t); ok && (pStat.Dev == s.Dev) && (pStat.Rdev == s.Rdev) && (pStat.Ino == s.Ino) {
-					return devDir + "/" + fi.Name(), nil
-				}
-			}
-
-			if e2 == io.EOF {
-				break
-			}
-
-			if e2 != nil {
-				return "", e2
-			}
-		}
-	}
-
-	return "", os.ErrNotExist
-}
-
-func GetTtyPath(pF *os.File) (string, error) {
-
-	info, E := pF.Stat()
-	if E != nil {
-		return "", E
-	}
-
-	if sys, ok := info.Sys().(*syscall.Stat_t); ok {
-
-		if path, e := findPtyDevByStat(sys); e == nil {
-			return path, nil
-		} else if os.IsNotExist(e) {
-			return "", E_NON_TTY
-		} else {
-			return "", e
-		}
-	}
-
-	return "", nil
-}
 
 func lcaseEnv(k string) string {
 	return strings.ToLower(strings.TrimSpace(os.Getenv(k)))
